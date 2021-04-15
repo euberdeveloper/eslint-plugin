f8f34517a5e028c7f2436b1434ee159f8489f7cd
euberdeveloper <euberdeveloper@gmail.com>
2021-04-16 00:01:58 +0200

    feat: add prettier
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const typescript = require('./configurations/typescript');
 const mocha = require('./configurations/mocha');
+const prettier = require('./configurations/prettier');
 
 module.exports = {
     configs: {
         typescript,
-        mocha
+        mocha,
+        prettier
     }
 };
