af80a353beeac9deface219c0f6100c74c879d17
euberdeveloper <euberdeveloper@gmail.com>
2022-04-03 20:44:11 +0200

    refactor: add duplication of packages
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ scripts/make-full.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const childProcess = require('child_process');
+
+const packageJsonPath = path.join(__dirname, '..', 'package.json');
+const packageJson = require(packageJsonPath);
+packageJson.name = 'eslint-plugin-euberdeveloper-full';
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+const devDependenciesPath = path.join(__dirname, 'dev-dependencies.txt');
+const devDependencies = fs.readFileSync(devDependenciesPath, 'utf8');
+const devDependenciesText = devDependencies.split('\n').map(line => line.trim()).filter(line => !!line).join(' ');
+const devDependenciesCommand = `npm i -D ${devDependenciesText}`;
+childProcess.execSync(devDependenciesCommand);
