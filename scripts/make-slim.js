409f8881d43486e020807c51011ed0f82bce9a19
euberdeveloper <euberdeveloper@gmail.com>
2022-04-03 21:16:58 +0200

    feat(scripts): update readme for slim
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- scripts/make-slim.js
+++ scripts/make-slim.js
@@ -7,3 +7,8 @@ packageJson.name = '@euberdeveloper/eslint-plugin-slim';
 packageJson.dependencies = undefined;
 packageJson.devDependencies = undefined;
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+const slimReadmePath = path.join(__dirname, '..', 'README-slim.md');
+const readmePath = path.join(__dirname, '..', 'README.md');
+const slimContent = fs.readFileSync(slimReadmePath, 'utf8');
+fs.writeFileSync(readmePath, slimContent);
